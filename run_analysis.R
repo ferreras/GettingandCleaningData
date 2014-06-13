0fbefa32174c05eba3fc2d1e81e3586a6606e8b6
Antonio Ferreras <ferreras@ferrerasdell.hi.inet>
2014-06-13 20:23:15 +0200

    First Commit - Code done

---
+++ run_analysis.R
@@ -0,0 +1,45 @@
+# Getting and Cleaning Data - Course Project
+# June 2014
+
+library(reshape2)
+
+# Read 561 Variable Data Names
+features <- scan(".//UCI HAR Dataset//features.txt", what=character(),
+                 sep="\n", quiet = TRUE) # Nombres de columnas
+
+# Read variable data and name the variables
+x_test <- read.table(".//UCI HAR Dataset//test//X_test.txt", header=FALSE, 
+                      colClasses="numeric", col.names = features)
+x_train <- read.table(".//UCI HAR Dataset//train//X_train.txt", header=FALSE,
+                       colClasses="numeric", col.names = features)
+
+# Extract only variables "mean" and "std"
+cols = names(x_test)[grep("mean|std",names(x_test))]
+x <- rbind(x_train[,cols],x_test[,cols])
+
+# Read Activity data, use descriptive names and merge into one column
+act <- read.table(".//UCI HAR Dataset//activity_labels.txt", header=FALSE)
+y_train <- read.table(".//UCI HAR Dataset//train//y_train.txt", header=FALSE, 
+                       colClasses="integer", col.names="activity")
+y_test <- read.table(".//UCI HAR Dataset//test//y_test.txt",
+                      header=FALSE, colClasses="integer", col.names="activity")
+y <- transform(rbind(y_train, y_test), 
+               activity=factor(activity, levels=c(1:6), labels=act[,2]))
+
+# Read subject indentifier and merge them
+s_train <- read.table(".//UCI HAR Dataset//train//subject_train.txt",
+                      header=FALSE, colClasses="integer", col.names="subject")
+s_test <- read.table(".//UCI HAR Dataset//test//subject_test.txt",
+                     header=FALSE, colClasses="integer", col.names="subject")    
+s <- transform(rbind(s_train, s_test), subject=factor(subject))
+
+# Merge all the data: subject, activity and vars
+total <- cbind(s, y, x)
+
+# Creates a second, independent tidy data set with the average of each 
+# variable for each activity and each subject. 
+
+totalMelt <- melt(data=total,id=c("subject","activity"), measure.vars=cols)
+
+totaltidy <- dcast(totalMelt, subject + activity ~ variable, mean)
+
